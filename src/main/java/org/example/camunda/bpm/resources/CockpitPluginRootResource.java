c6e344711f24467691a8274bf728f3520b487b93
cz2803 <c.zumpf@jit.at>
2022-02-02 13:27:41 +0100

    Implementation of retry via Camunda Java API

--- src/main/java/org/example/camunda/bpm/resources/CockpitPluginRootResource.java
+++ src/main/java/org/example/camunda/bpm/resources/CockpitPluginRootResource.java
@@ -3,14 +3,20 @@ package org.example.camunda.bpm.resources;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-import okhttp3.*;
 import org.camunda.bpm.cockpit.plugin.resource.AbstractCockpitPluginRootResource;
 
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.management.JobDefinition;
 import org.example.camunda.bpm.CockpitPlugin;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Path("plugin/" + CockpitPlugin.ID)
+@Component
 public class CockpitPluginRootResource extends AbstractCockpitPluginRootResource {
 
   public CockpitPluginRootResource() {
@@ -19,17 +25,17 @@ public class CockpitPluginRootResource extends AbstractCockpitPluginRootResource
 
   @Path("{engineName}/process-instance")
   public ProcessInstanceResource getProcessInstanceResource(@PathParam("engineName") String engineName) throws IOException {
-    //Camunda Rest API Call is done here
-    OkHttpClient client = new OkHttpClient().newBuilder()
-            .build();
-    MediaType mediaType = MediaType.parse("application/json");
-    RequestBody body = RequestBody.create(mediaType, "{\r\n    \"processInstanceQuery\": {\r\n        \"processDefinitionIds\": [\r\n            \"test-process\"\r\n        ]\r\n    },\r\n    \"retries\": 1\r\n}");
-    Request request = new Request.Builder()
-            .url("http://localhost:8080/engine-rest/process-instance/job-retries")
-            .method("POST", body)
-            .addHeader("Content-Type", "application/json")
-            .build();
-    Response response = client.newCall(request).execute();
+
+    ProcessEngine processEngine = ProcessEngines.getProcessEngine(engineName);
+
+    ManagementService managementService = processEngine.getManagementService();
+
+    List<JobDefinition> jobDefinitions = managementService.createJobDefinitionQuery().processDefinitionKey("some-process").list();
+
+    for (JobDefinition jde : jobDefinitions) {
+      managementService.setJobRetriesByJobDefinitionId(jde.getId(), 1);
+    }
+
     return subResource(new ProcessInstanceResource(engineName), engineName);
   }
 }
